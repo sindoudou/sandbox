3bcbaada124e23bd9c0bf85865594f9f38d69c9b
zainab-ali <zainab.ali.london@gmail.com>
2016-08-13 13:03:30 +0100

    First cut

---
+++ project/plugins.sbt
@@ -0,0 +1,1 @@
+addSbtPlugin("sbt-directive" %% "sbt-directive" % "0.0.1-SNAPSHOT")
---
+++ src/main/scala/sandbox/foo.scala
@@ -0,0 +1,12 @@
+package sandbox
+
+import fs2._
+
+object TestApp extends App {
+
+  println("Hello!")
+
+//  def main(): Unit = {
+  //  println("Hello world!")
+ // }
+}
